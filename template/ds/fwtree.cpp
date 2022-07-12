8f21edb84680c9b775a75f3c1df51b9919689945
rogeryoungh <rogeryoungh@users.noreply.github.com>
2022-07-12 09:01:48 +0000

    deploy: e7e3ce4ad73e7ad9d5efd5b7ebdb348d6d7da9a1

--- template/ds/fwtree.cpp
+++ template/ds/fwtree.cpp
@@ -3,28 +3,28 @@ using namespace std;
 
 // @description 树状数组
 // @problem https://judge.yosupo.jp/problem/point_add_range_sum
+// @problem https://loj.ac/p/130
 
 template <class T>
 struct fwtree {
+	int n;
 	vector<T> v;
-	fwtree(int n = 0) : v(n) {}
-	int len() const {
-		return v.size();
-	}
+	fwtree(int n_ = 0) : n(n_), v(n) {}
 	void add(int i, T x) {
-		for (; 0 < i && i < len(); i += i & -i)
+		for (; 0 < i && i < n; i += i & -i) {
 			v[i] += x;
+		}
 	}
 	void init(const vector<T> &a) {
-		for (int i = 1; i < len(); i++) {
+		for (int i = 1; i < n; i++) {
 			v[i] += a[i];
 			int j = i + (i & -i);
-			if (j < len())
+			if (j < n)
 				v[j] += v[i];
 		}
 	}
 	T sum(int i) const {
-		T sum = 0;
+		T sum = T();
 		for (; i > 0; i -= i & -i)
 			sum += v[i];
 		return sum;
