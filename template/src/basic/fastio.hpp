884644f38765ba1be1ea9aa432aa6d88855c992f
rogeryoungh <rogeryoungh@users.noreply.github.com>
2022-11-22 06:41:04 +0000

    deploy: d6f348ae67b1b17eb962a4e34f95dc58e0365520

--- template/src/basic/fastio.hpp
+++ template/src/basic/fastio.hpp
@@ -1,52 +1,82 @@
 #include "basic/index.hpp"
 
-struct FastI {
-	char s[1 << 20], *t = s;
-	FastI() {
-		fread(s, 1, sizeof(s), stdin);
+struct BasicBuffer {
+	std::vector<char> s;
+	BasicBuffer() : s(1 << 18) {}
+	char *p = s.data(), *beg = p, *end = p + s.size();
+	inline char getc() {
+		if (p == end)
+			readAll();
+		return *p++;
 	}
-	template <class T, bool is_int = is_integral<T>::value>
-	FastI &operator>>(T &x) {
-		x = 0;
-		while (!isdigit(*t))
-			t++;
-		while (isdigit(*t))
-			x = x * 10 + *t++ - '0';
-		return *this;
+	inline void putc(char c) {
+		if (p == end)
+			writeAll();
+		*p++ = c;
+	}
+	inline void puts(const char *x) {
+		while (*x != 0)
+			putc(*x++);
+	}
+	void readAll() {
+		std::fread(beg, 1, end - beg, stdin);
+		p = s.data();
+	}
+	void writeAll() {
+		std::fwrite(beg, 1, p - beg, stdout);
+		p = s.data();
 	}
 };
 
-struct FastO {
-	char s[1 << 20], *t = s;
+struct FastI : BasicBuffer {
+	FastI() {
+		readAll();
+	}
+	ll read() {
+		ll x = 0;
+		char c = getc();
+		bool sgn = true;
+		while (!std::isdigit(c))
+			sgn = sgn && c != '-', c = getc();
+		while (std::isdigit(c))
+			x = x * 10 + c - '0', c = getc();
+		return sgn ? x : -x;
+	}
+	template <class T>
+	FastI &operator>>(T &x) {
+		return x = read(), *this;
+	}
+	FastI &operator>>(char &x) {
+		return x = getc(), *this;
+	}
+};
+
+struct FastO : BasicBuffer {
+	std::array<char, 32> u{};
 	~FastO() {
-		fwrite(s, 1, t - s, stdout);
+		writeAll();
 	}
-	void puts(const char *x) {
-		while (*x != 0)
-			*t++ = *x++;
-	}
-	void output(uint64_t x) {
-		static char u[22];
-		char *i = u + 20;
-		do {
+	void output(ll x) {
+		char *i = u.data() + 20;
+		if (x < 0)
+			putc('-'), x = -x;
+		do
 			*--i = x % 10 + '0', x /= 10;
-		} while (x > 0);
+		while (x > 0);
 		puts(i);
 	}
-	FastO &operator<<(uint64_t x) {
+	template <class T>
+	FastO &operator<<(T &x) {
 		return output(x), *this;
 	}
 	FastO &operator<<(char x) {
-		return *t++ = x, *this;
-	}
-	FastO &operator<<(int64_t x) {
-		if (x < 0) {
-			*--t = '-', x = -x;
-		}
-		return output(x), *this;
+		return putc(x), *this;
 	}
 	FastO &operator<<(const char *x) {
 		return puts(x), *this;
+	}
+	FastO &operator<<(const std::string &x) {
+		return puts(x.c_str()), *this;
 	}
 };
 
