1eaa67f79e256f1ce7e295eda339ad503dd4e73d
rogeryoungh <rogeryoungh@users.noreply.github.com>
2022-11-28 13:36:23 +0000

    deploy: bfe0000de95636802aa41dfe0f10688ac798d05a

--- math-P/interpolation/lagrange1.hpp
+++ math-P/interpolation/lagrange1.hpp
@@ -4,8 +4,8 @@
 
 // @description 拉格朗日插值
 
-ll lagrange(const V<ll> &x, const V<ll> &y, ll k) {
-	ll sum = 0, n = x.size();
+int lagrange(const V<int> &x, const V<int> &y, int k) {
+	int sum = 0, n = x.size();
 	for (int i = 0; i < n; i++) {
 		int s1 = 1, s2 = 1;
 		for (int j = 0; j < n; j++) {
--- math-modint/interpolation/lagrange1.hpp
+++ math-modint/interpolation/lagrange1.hpp
@@ -9,11 +9,9 @@ Z lagrange(const V<Z> &x, const V<Z> &y, Z k) {
 	Z sum = 0;
 	for (int i = 0; i < n; i++) {
 		Z s1 = 1, s2 = 1;
-		for (int j = 0; j < n; j++) {
-			if (j == i)
-				continue;
-			s1 *= k - x[j], s2 *= x[i] - x[j];
-		}
+		for (int j = 0; j < n; j++)
+			if (j != i)
+				s1 *= k - x[j], s2 *= x[i] - x[j];
 		sum += s1 / s2 * y[i];
 	}
 	return sum;
