b200bc9926271b21f13884c8fb8bc2c9f3d58dfb
rogeryoungh <rogeryoungh@foxmail.com>
2024-03-12 13:33:02 +0800

    update: katex use pre-render and cdn

--- scripts/build-template-docs.sh
+++ scripts/build-template-docs.sh
@@ -3,13 +3,12 @@
 mkdir -p $HOME/bin
 
 curl -sSL https://github.com/rust-lang/mdBook/releases/download/v0.4.37/mdbook-v0.4.37-x86_64-unknown-linux-gnu.tar.gz | tar -xz --directory=$HOME/bin
-wget -q https://github.com/rogeryoungh/preprocessor-md-tex/releases/download/v0.1.2/preprocessor-md-tex -O $HOME/bin/preprocessor-md-tex
+curl -sSL https://github.com/lzanini/mdbook-katex/releases/download/v0.6.0/mdbook-katex-v0.6.0-x86_64-unknown-linux-gnu.tar.gz | tar -xz --directory=$HOME/bin
 
 chmod +x $HOME/bin/mdbook
-chmod +x $HOME/bin/preprocessor-md-tex
+chmod +x $HOME/bin/mdbook-katex
 
 export PATH="$HOME/bin:$PATH"
 
-preprocessor-md-tex ./template/src/
 python ./scripts/extract.py
 cd ./template && mdbook build
